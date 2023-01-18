07e52348b38a6b874c697497d984fd2567c37102
clybb1 <lyb20004@byui.edu>
2023-01-18 13:36:32 -0700

    server.js, package.json, and login.cmd updates

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+
 const app = express();
+
 const port = 3000;
+
+const bodyParser = require ("body-parser");
+
+app.use(bodyParser.json()); // activates body-parser to look for incoming data
+
 app.get("/", (req, res) => {
     res.send("Hello Caleb");
 });
+
+app.post('/login', (req, res) =>{
+    const loginUser = req.body.userName;
+    console.log('Login username:'+loginUser);
+    res.send('Hello ' +loginUser);
+})
+
 app.listen(port, ()=> {
     console.log("listening");
 });
