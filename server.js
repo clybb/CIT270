9145ab955b3dff5fd586b41085a19dcc81725aa3
clybb1 <lyb20004@byui.edu>
2023-01-20 13:39:52 -0700

    changes to server.js and install of uuid

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const bodyParser = require ("body-parser");
 
+const {v4: uuidv4} = require('uuid');
+
 app.use(bodyParser.json()); // activates body-parser to look for incoming data
 
 app.get("/", (req, res) => {
@@ -14,8 +16,16 @@ app.get("/", (req, res) => {
 
 app.post('/login', (req, res) =>{
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password;
     console.log('Login username:'+loginUser);
-    res.send('Hello ' +loginUser);
+    if (loginUser=="shady.construction@gmail.com" && loginPassword=="P@ssw0rd"){
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    }
+    else {
+        res.status(401);
+        res.send("Inncorrect password for "+loginUser);
+    }
 })
 
 app.listen(port, ()=> {
