54747664983406904e78caefe6d765195d158115
clybb1 <lyb20004@byui.edu>
2023-01-27 13:46:42 -0700

    ui addition

--- server.js
+++ server.js
@@ -14,16 +14,19 @@ const {v4: uuidv4} = require('uuid');
 
 app.use(bodyParser.json()); // activates body-parser to look for incoming data
 
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
     res.send("Hello Caleb");
 });
 
-app.post('/login', (req, res) =>{
+app.post('/login', async (req, res) =>{
     const loginUser = req.body.userName;
     const loginPassword = req.body.password;
     console.log('Login username:'+loginUser);
-    if (loginUser=="shady.construction@gmail.com" && loginPassword=="P@ssw0rd"){
+    const correctPassword = await redisClient.hGet('UserMap', loginUser);
+    if (loginPassword==correctPassword){
         const loginToken = uuidv4();
         res.send(loginToken);
     }
