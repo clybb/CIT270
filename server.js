c36ed96dd70e96d9f8e9b2ced49cbee1e154bd7d
clybb1 <lyb20004@byui.edu>
2023-02-01 13:49:49 -0700

    edits

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
 const {v4: uuidv4} = require('uuid');
 app.use(bodyParser.json()); // activates body-parser to look for incoming data
 app.use(express.static("public"));
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello Caleb");
 });
-app.get("/validate/:loginToken", async(req, res) =>{
-    const loginToken = req.params.loginToken;
+app.get("/validate", async(req, res) =>{
+    const loginToken = req.cookies.stedicookie;
+    console.log("loginToken", loginToken);
     const loginUser = await redisClient.hGet('TokenMap', loginToken);
     res.send(loginUser);
 });
